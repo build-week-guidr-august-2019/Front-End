26ebc884534fb4cd9c4d5698c3fbf8f22943ed5c
Zach Taylor <zbtaylor1@gmail.com>
2019-08-21 23:01:19 -0400

    stubbed out a component structure to start us off

---
+++ src/components/trip/TripCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Trip summary meant for embedding in the portfolio list view.
+
+const TripCard = () => {
+  return (
+    <div>TripCard</div>
+  )
+}
+
+export default TripCard;
---
+++ src/components/trip/TripEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Include TripForm and any trip editing specific logic.
+
+const TripEdit = () => {
+  return (
+    <div>TripEdit</div>
+  )
+}
+
+export default TripEdit;
