56dcbb12e170472ec077dd76904bcb25939f2939
Zach Taylor <zbtaylor1@gmail.com>
2019-08-22 23:15:09 -0400

    home and trip links added to menu

--- src/components/page/Header.js
+++ src/components/page/Header.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 const Header = () => {
-  return ()
-}
+  return (
+    <Menu>
+      <Menu.Item as={NavLink} name="home" to="/" exact />
+      <Menu.Item as={NavLink} name="trips" to="/trips" />
+    </Menu>
+  );
+};
 
 export default Header;
