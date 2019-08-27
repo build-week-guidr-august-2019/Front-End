0156bf62a77cfa69eb8ada8774953b0c5df0d961
Zach Taylor <zbtaylor1@gmail.com>
2019-08-27 00:01:06 -0400

    added portfolio route, sending history to user auth, redirecting user on successful login

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import UserLogin from "./components/user/UserLogin";
 import UserCreate from "./components/user/UserCreate";
+import PortfolioList from "./components/portfolio/PortfolioList";
 
 const Routes = () => {
   return (
     <>
       <Route path="/" exact component={UserLogin} />
       <Route path="/register" exact component={UserCreate} />
+      <Route path="/portfolio" exact component={PortfolioList} />
     </>
   );
 };
