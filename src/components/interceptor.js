8775a472c161fd5d01d14338f9de9411b2268915
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-08-26 20:55:42 -0500

    Added interceptor

---
+++ src/components/interceptor.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// URL shared by every endpoint
+axios.defaults.baseURL = "https://lambda-guidr.herokuapp.com/api";
+
+axios.interceptors.request.use(config => {
+  // Add a token to every outgoing request
+  const token = localStorage.getItem("token");
+  config.headers.authorization = token;
+  return config;
+});
+
+axios.interceptors.response.use(res => {
+  // Strip the token off of any incoming response (if it exists) and
+  // Add it to storage
+  if (res.data.token) {
+    localStorage.setItem("token", res.data.token);
+  }
+  return res;
+});
