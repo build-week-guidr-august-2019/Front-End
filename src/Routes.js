4991f4c7e548d5621168d2431a7d743bdb718597
Zach Taylor <zbtaylor1@gmail.com>
2019-08-28 15:14:46 -0400

    basic private routes, always redirects to login. need to integrate with the login form to start seeing other components

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,36 @@
 import React, { Fragment } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import UserLogin from "./components/user/UserLogin";
 import UserCreate from "./components/user/UserCreate";
 import Dashboard from "./components/page/dashboard";
 import TripCreate from "./components/trip/TripCreate";
 import TripDetail from "./components/trip/TripDetail";
 
+const isLoggedIn = false;
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isLoggedIn === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
 const Routes = () => {
   return (
     <>
       <Route path="/login" exact component={UserLogin} />
-      <Route path="/register" exact component={UserCreate} />
-      <Route path="/dashboard" exact component={Dashboard} />
-      <Route path="/trips/add" exact component={TripCreate} />
-      <Route path="/trips/:id" component={TripDetail} />
+      <PrivateRoute path="/register" exact component={UserCreate} />
+      <PrivateRoute path="/dashboard" exact component={Dashboard} />
+      <PrivateRoute path="/trips/add" exact component={TripCreate} />
+      <PrivateRoute path="/trips/:id" component={TripDetail} />
     </>
   );
 };
