fe060e2239087132367c56f0f6d440846f778588
Zach Taylor <zbtaylor1@gmail.com>
2019-08-28 12:33:12 -0400

    added dummy post data and getting successful trip create response after registering user

--- src/components/trip/TripCreate.js
+++ src/components/trip/TripCreate.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Form, Field, withFormik } from "formik";
+import Axios from "axios";
 import * as Yup from "yup";
 
 import newTrip from "../../img/trip-pic-1.jpg";
@@ -96,6 +97,23 @@ const newTripSubmission = withFormik({
       .min(200, "200 characters required")
   }),
   handleSubmit(values, { setStatus, resetForm }) {
+    const trip = {
+      user_id: 5,
+      title: values.title,
+      shortDescription: "short description",
+      description: values.description,
+      isProfessional: true,
+      type: 1,
+      duration: 3,
+      distance: 500,
+      date: "2018-11-01"
+    };
+
+    Axios.post("https://lambda-guidr.herokuapp.com/api/trip", trip).then(
+      res => {
+        console.log(res);
+      }
+    );
     alert("Your trip has been accepted", JSON.stringify(values, null, 2));
     resetForm();
   }
