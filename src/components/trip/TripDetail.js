10470576c5401d010b0e69ae85f861197b204e19
Zach Taylor <zbtaylor1@gmail.com>
2019-08-28 14:11:42 -0400

    trip detail successfully pulling from api + trip detail route

--- src/components/trip/TripDetail.js
+++ src/components/trip/TripDetail.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-// Full details for a specific trip.
+const TripDetail = props => {
+  const [trip, setTrip] = useState({});
 
-const TripDetail = () => {
+  useEffect(() => {
+    Axios.get(
+      `https://lambda-guidr.herokuapp.com/api/trip/${props.match.params.id}`
+    ).then(res => {
+      console.log(res);
+      setTrip(res.data);
+    });
+  }, []);
+
   return (
-    <div>TripDetail</div>
-  )
-}
+    <div>
+      <p>{trip.title}</p>
+      <p>{trip.date}</p>
+      <p>{trip.type}</p>
+      <p>{trip.duration}</p>
+      <p>{trip.description}</p>
+    </div>
+  );
+};
 
 export default TripDetail;
