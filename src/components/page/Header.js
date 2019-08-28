9c98493f80b7cba85edec034b9e74a2b2c7bc37e
Zach Taylor <zbtaylor1@gmail.com>
2019-08-27 21:26:59 -0400

    updated links in header

--- src/components/page/Header.js
+++ src/components/page/Header.js
@@ -5,8 +5,8 @@ import { Menu } from "semantic-ui-react";
 const Header = () => {
   return (
     <Menu>
-      <Menu.Item as={NavLink} name="home" to="/" exact />
-      <Menu.Item as={NavLink} name="add trip" to="/trips/add" icon="plus" />
+      <Menu.Item as={NavLink} name="home" to="/dashboard" exact />
+      <Menu.Item as={NavLink} name="add trip" to="/new-trip" icon="plus" />
       <Menu.Item as={NavLink} name="logout" to="/logout" position="right" />
     </Menu>
   );
