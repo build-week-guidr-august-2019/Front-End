5e1feb5f14a021d4752c1bc41955c3cb4ebcd193
Zach Taylor <zbtaylor1@gmail.com>
2019-08-29 23:08:18 -0400

    more formatting for trip detail and added edit link

--- src/components/trip/TripDetail.js
+++ src/components/trip/TripDetail.js
@@ -1,31 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, Image, List } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Container, Grid, Image, List, Button } from "semantic-ui-react";
 import Axios from "axios";
 import trip1 from "../../img/trip1.JPG";
 
 const TripDetail = props => {
   const [trip, setTrip] = useState({});
+  const id = props.match.params.id;
 
   useEffect(() => {
-    Axios.get(
-      `https://lambda-guidr.herokuapp.com/api/trip/${props.match.params.id}`
-    ).then(res => {
+    Axios.get(`https://lambda-guidr.herokuapp.com/api/trip/${id}`).then(res => {
       console.log(res);
       setTrip(res.data);
     });
   }, []);
 
   return (
-    <Grid columns={1} centered>
+    <Grid columns={2} centered>
       <Grid.Column width={8}>
         <Container>
           <h2>{trip.title}</h2>
-          <Image src={trip1} />
           <p>{trip.description}</p>
           <List>
             <List.Item>Date of Trip: {trip.date}</List.Item>
             <List.Item>Duration of Trip: {trip.duration} days</List.Item>
           </List>
+          <Button as={Link} to={`/trips/${id}/edit`}>
+            Edit Trip
+          </Button>
+        </Container>
+      </Grid.Column>
+      <Grid.Column width={8}>
+        <Container>
+          <Image src={trip1} />
         </Container>
       </Grid.Column>
     </Grid>
